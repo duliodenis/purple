a71e808166eb593a62ed0300e0d787ac5c382771
Dulio Denis <dulio.denis@yahoo.com>
2015-04-02 18:28:20 -0400

    Initial commit

---
+++ Purple/Purple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Purple
+//
+//  Created by Dulio Denis on 4/2/15.
+//  Copyright (c) 2015 Dulio Denis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# purple
-A delightful iOS game written in Swift where the goal is to get to purple.
+# Code name: Purple
+A delightfully colorful iOS game written in Swift where the goal is to get to purple.
+
+### Support or Contact
+Visit [ddApps.co](http://ddapps.co) to see more.
